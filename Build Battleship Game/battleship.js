4091214fb6c33332e8351246c85af225d55fe652
Vasil Donchev <vasilkd@gmail.com>
2022-03-28 17:48:53 +0300

    update battleship.js

--- Build Battleship Game/battleship.js
+++ Build Battleship Game/battleship.js
@@ -1,31 +1,36 @@
+let location1 = 3,
+  location2 = 4,
+  location3 = 5; // holds location of the battleship
+let guess; // hold the usre's current guess
+let guesses = 0; // hold the number of guesses
+let hits = 0; // hold the number of hits
+let isSunk = false; // keep track of whether the ship is sunk ot not
 
-let location1 = 3, location2 = 4, location3 = 5; // holds location of the battleship
-let guess;  // hold the usre's current guess
-let guesses = 0;    // hold the number of guesses
-let hits = 0;   // hold the number of hits
-let isSunk = false; // keep track of whether the ship is sunk ot not 
-
-// LOOP while the ship is  not sunk
+// LOOP while the ship is not sunk
 while (isSunk == false) {
     // GET  the user's guess
-    guess = prompt('Ready, aim, fire! (enter a number from 0-6):');
+    guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
     // COMPARE the user's input to valid  input values
     if (guess < 0 || guess > 6) {
-        // IF the user's guess iis invalid,
-        //      - TELL the user to enter valid number
-        alert('Please enter a valid cell number!');
-    } else {        //  ELSE
-        guesses++   //      - ADD one to guesses
-    }                //    IF the user's guess matches a location 
-       if (guess == location1 || guess == location2 || guess == location3) {           
-            hits++; //  ADD one to hits
-        //          IF number of hits is 3
-        //              SET isSunk to true
-        //              TELL user "You sank my battleship!"
-        //          END IF
-        //      END IF
-     
-    }    //  END ELSE
-}   // END LOOP
-
+        // IF the user's guess is invalid, - TELL the user to enter valid number
+        alert("Please enter a valid cell number!");
+    } else {
+        guesses = guesses + 1; // - ADD one to guesses
+        //  IF the user's guess matches a location
+        if (guess == location1 || guess == location2 || guess == location3) {
+            hits = hits + 1; //  ADD one to hits
+            alert("HIT!");
+            //  IF number of hits is 3
+            if (hits == 3) {
+                isSunk = true; // SET isSunk to true
+                alert("You sank my battleship"); //  TELL user "You sank my battleship!"
+            }
+        } else {
+            alert("MISS");
+        }
+    }
+}
 // TELL user stats
+let stats = "You took " + guesses + " guesses to sink the battleship, " + 
+"which means your shooting accuracy was " + 3 / guesses;
+alert(stats);
