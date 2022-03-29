4e53e3bf1bfd5be4a1b8351f0ce31b3ded85ebe3
Vasil Donchev <vasilkd@gmail.com>
2022-03-29 23:02:04 +0300

    1. rename to Bbbles-A-US; 2. refactor to use functions to do calculations

--- arrays/Bubbles-A-Us.js
+++ arrays/Bubbles-A-Us.js
@@ -1,11 +1,12 @@
-function bubblesTest() {
-  let scores = [
-    60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69,
-    64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44,
-  ];
-  let output;
+let scores = [
+  60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69,
+  64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44,
+];
+
+function printAndGetHighScore(arr) {
   let testNumbers = scores.length;
   let highestScore = 0;
+  let output;
 
   for (let i = 0; i < scores.length; i++) {
     output = "Bubble solution #" + i + " score:" + scores[i];
@@ -16,7 +17,10 @@ function bubblesTest() {
   }
   console.log(`Bubbles test: ${testNumbers}`);
   console.log("Highest bublle score: " + highestScore);
+  return highestScore;
+}
 
+function getBestResults(arr) {
   let hiScoreIndeces = [];
   for (let i = 0; i < scores.length; i++) {
     if (scores[i] == highestScore) {
@@ -25,4 +29,5 @@ function bubblesTest() {
   }
   console.log("Solutions with highest score: " + hiScoreIndeces);
 }
-bubblesTest();
+
+getBestResults(printAndGetHighScore(scores));
